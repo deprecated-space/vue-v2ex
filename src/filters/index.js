99a91db6e1180053fc233e9456f02cab955af697
hanzichi <hanzichi@outlook.com>
2016-12-22 17:56:34 +0800

    Add Login & Register Page

--- src/filters/index.js
+++ src/filters/index.js
@@ -6,3 +6,23 @@ export function formatDate(timestamp) {
 
   return year + '-' + month + '-' + day;
 }
+
+// export function formatDate2(timestamp) {
+//   let date = new Date(timestamp * 1000)
+//     , year = date.getFullYear()
+//     , month = date.getMonth() + 1
+//     , day = date.getDate();
+
+//   return year + '|' + month + '|' + day;
+// }
+
+// export {
+//   formateDate: function(timestamp) {
+//     let date = new Date(timestamp * 1000)
+//       , year = date.getFullYear()
+//       , month = date.getMonth() + 1
+//       , day = date.getDate();
+
+//     return year + '-' + month + '-' + day;
+//   }
+// }
